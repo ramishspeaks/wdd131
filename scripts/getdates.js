680730ee41aedec8a0a5982841653c80864df799
ramishspeaks <rkhokhar@byupathway.edu>
2026-05-16 15:15:39 +0500

    Updated WDD131 homepage assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent =
-`Last Modified: ${document.lastModified}`;
+`Last Modification: ${document.lastModified}`;
