ae8968ffccddd22821ec155a633a2170e3768934
ramishspeaks <rkhokhar@byupathway.edu>
2026-05-17 01:14:34 +0500

    Completed Week 02 DOM activity

---
+++ Week02/scripts/bom.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#favchap');
+
+const button = document.querySelector('button');
+
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', function () {
+
+    if (input.value !== '') {
+
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        deleteButton.setAttribute('aria-label', 'Remove Chapter');
+
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+        });
+
+
+        input.value = '';
+
+        input.focus();
+    }
+
+});
