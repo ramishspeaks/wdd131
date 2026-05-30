bd6fb8224655734f8a6ade85851061471b544d31
ramishspeaks <rkhokhar@byupathway.edu>
2026-05-31 02:58:28 +0500

    Completed W03 Country Page assignment

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
+    ).toFixed(1);
+}
+
+const temperature = 30;
+const windSpeed = 10;
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent =
+        calculateWindChill(temperature, windSpeed) + " °C";
+} else {
+    windChill.textContent = "N/A";
+}
